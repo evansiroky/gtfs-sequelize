7f4717dc5bef5a28507a3090c576d92384589b1a
djstroky <djstroky@yahoo.com>
2015-09-12 12:13:29 -0700

    Dumping request dependency

--- tests/testDownload.js
+++ tests/testDownload.js
@@ -1,8 +1,8 @@
 var Q = require('q');
 
 var downloadConfig = {
-  // gtfsUrl: 'http://feed.rvtd.org/googleFeeds/static/google_transit.zip' // RVTD - small agency
-  gtfsUrl: 'http://www.vta.org/sfc/servlet.shepherd/document/download/069A0000001NUea',  // VTA - large agency
+  gtfsUrl: 'http://feed.rvtd.org/googleFeeds/static/google_transit.zip', // RVTD - small agency
+  //gtfsUrl: 'http://www.vta.org/sfc/servlet.shepherd/document/download/069A0000001NUea',  // VTA - large agency
   downloadsDir: 'downloads'
 }
 
