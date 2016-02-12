bb414dde619c065c535e962e05c43bdd96f5e0b9
evansiroky <evan.siroky@yahoo.com>
2016-02-12 14:43:18 -0800

    Missing agency_id bug fix

--- tests/testDownload.js
+++ tests/testDownload.js
@@ -1,7 +1,7 @@
 var Promise = require('bluebird');
 
 var downloadConfig = {
-  gtfsUrl: 'http://feed.rvtd.org/googleFeeds/static/google_transit.zip', // RVTD - small agency
+  gtfsUrl: 'http://scmtd.com/google_transit/google_transit.zip', // RVTD - small agency
   //gtfsUrl: 'http://www.vta.org/sfc/servlet.shepherd/document/download/069A0000001NUea',  // VTA - large agency
   downloadsDir: 'downloads'
 }
